2300de09956eb185aa896fc1249a92526f0f67c3
Casie Siekman <casie.siekman@gmail.com>
2020-10-07 16:14:08 -0500

    add AJAX

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,29 @@
 console.log("hello from JS");
+
+$(document).ready(onReady);
+
+function onReady(){
+    console.log('hello from jquery');
+    // perform GET request
+    getRandomQuote();
+}
+
+function getRandomQuote(){
+    console.log("get the quote");
+    $.ajax({
+        method: 'GET',
+        url: '/randomQuote'
+    }).then(function(response){
+        console.log('response', response);
+        appendToDom(response);
+    });
+}
+
+function appendToDom(dataToAppend){
+    // take the response from the server
+    // append it to #output so it shows up on the DOM
+    $('#output').append(`
+        <p>${dataToAppend.quote}</p>
+        <i>from: ${dataToAppend.author}</i>
+    `)
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Require express - gives us a function
 const express = require('express');
-
+// require body-parser to parse through data
+const bodyParser = require('body-parser');
 // Create an instance of express by calling the function 
 // returned above - gives us an object
 const app = express();
@@ -11,22 +12,30 @@ const quotesData = require ('./modules/quotes.js');
 // express static file serving - public is the folder name
 app.use(express.static('server/public'));
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 let index = 0;
 
 app.get('/quotes', (req, res) => {
     console.log('hi from get request');
-    res.send(quotesData);
+    res.send(quotesData.list);
 })
 
 app.get('/randomQuote', (req, res) => {
-    let randomNumber = getRandomInt(quotesData.length);
-    res.send(quotesData[randomNumber]);
+    let randomNumber = getRandomInt(quotesData.list.length);
+    res.send(quotesData.list[randomNumber]);
 });
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
-  }
+}
+
+app.post('/quotes', (req, res) => {
+    console.log('hello from post', req.body);
+    //push req.body to our quotesData array
+    quotesData.list.push(req.body);
+    res.sendStatus(200);
+});
 
 app.listen(port, () => {
     console.log("Up and running on port: ", port);
